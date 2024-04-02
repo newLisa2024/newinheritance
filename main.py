9635ef7063c5d4b18fa81cedb8ba06e901230e14
newLisa2024 <165168788+newLisa2024@users.noreply.github.com>
2024-04-02 13:30:29 +0300

    new baza main

---
+++ main.py
@@ -0,0 +1,20 @@
+class Bird():
+    def __init__(self, name, voice, color):
+        self.name = name
+        self.voice = voice
+        self.color = color
+
+    def fly(self):
+        print(f"{self.name} летает")
+
+    def eat(self):
+        print(f"{self.name} кушает")
+
+    def sing(self):
+        print(f"{self.name} поёт - {self.voice}")
+
+    def info(self):
+        print(f"{self.name} - имя")
+        print(f"{self.voice} - голос")
+        print(f"{self.color} - окрас")
+        
